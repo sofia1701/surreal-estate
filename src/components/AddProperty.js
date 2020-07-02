29710f1b6b0bcf34a1db2ad23a3ffbb3be44f80c
Sofia <sofia.dionisio94@gmail.com>
2020-07-02 13:39:51 +0100

    Completed AddProperty form with remaining required fields, added some styles

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -7,6 +7,10 @@ export default function AddProperty() {
       title: "",
       city: "Manchester",
       type: "Flat",
+      bedrooms: "1",
+      bathrooms: "1",
+      price: "",
+      email: "",
     },
   };
 
@@ -32,6 +36,7 @@ export default function AddProperty() {
           Title
           <input
             className="input-fields"
+            type="text"
             placeholder="Add a title..."
             id="title"
             name="title"
@@ -74,6 +79,64 @@ export default function AddProperty() {
             <option value="Bungalow">Bungalow</option>
           </select>
         </label>
+        <label htmlFor="bedrooms">
+          Bedrooms
+          <select
+            data-testid="select-bedroom-id"
+            className="input-fields"
+            id="bedrooms"
+            name="bedrooms"
+            value={fields.bedrooms}
+            onChange={handleFieldChange}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5+">5+</option>
+          </select>
+        </label>
+        <label htmlFor="bathrooms">
+          Bathrooms
+          <select
+            data-testid="select-bathrooms-id"
+            className="input-fields"
+            id="bathrooms"
+            name="bathrooms"
+            value={fields.bathrooms}
+            onChange={handleFieldChange}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4+">4+</option>
+          </select>
+        </label>
+        <label htmlFor="price">
+          Price (£)
+          <input
+            className="input-fields"
+            placeholder="Add a price..."
+            type="number"
+            id="price"
+            name="price"
+            value={fields.price}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            className="input-fields"
+            placeholder="Add an email..."
+            type="email"
+            id="email"
+            name="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </label>
+
         <button className="add-button" type="submit">
           Add
         </button>
