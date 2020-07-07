bd2fe70747821c8dccb95105c8b4eeb5f2f7093d
Sofia <sofia.dionisio94@gmail.com>
2020-07-07 16:54:25 +0100

    Added function buildQueryString in Sidebar component in order to merge two query strings, one to sort by city and the other to sort by ascending or descending price

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,64 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 import "../styles/sidebar.css";
 
 export default function Sidebar() {
+  const { search } = useLocation();
+
+  const buildQueryString = (operation, valueObj) => {
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj),
+    };
+
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
   return (
     <div className="sidebar">
       <p className="sidebar-title">Filter by city</p>
       <div className="items">
-        <Link className="side-item" to={`/?query={"city": "Manchester"}`}>
+        <Link
+          to={buildQueryString("query", { city: "Manchester" })}
+          className="side-item"
+        >
           Manchester
         </Link>
-        <Link className="side-item" to={`/?query={"city": "Leeds"}`}>
+        <Link
+          to={buildQueryString("query", { city: "Leeds" })}
+          className="side-item"
+        >
           Leeds
         </Link>
-        <Link className="side-item" to={`/?query={"city": "Sheffield"}`}>
+        <Link
+          to={buildQueryString("query", { city: "Sheffield" })}
+          className="side-item"
+        >
           Sheffield
         </Link>
-        <Link className="side-item" to={`/?query={"city": "Liverpool"}`}>
+        <Link
+          to={buildQueryString("query", { city: "Liverpool" })}
+          className="side-item"
+        >
           Liverpool
+        </Link>
+      </div>
+      <p className="sidebar-title">Sort by</p>
+      <div className="items">
+        <Link to={buildQueryString("sort", { price: 1 })} className="side-item">
+          Price Ascending
+        </Link>
+        <Link
+          to={buildQueryString("sort", { price: -1 })}
+          className="side-item"
+        >
+          Price Descending
         </Link>
       </div>
     </div>
