343c45fc536523fdfb963961305fa479486a81ef
Sofia <sofia.dionisio94@gmail.com>
2020-07-16 11:38:59 +0100

    Added basic tests for Homepage and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/footer.css";
 
 function Footer() {
-  return <div className="footer">Sofia Dionisio, Manchester Codes 2020</div>;
+  return (
+    <div className="footer" data-testid="footer-id">
+      Sofia Dionisio, Manchester Codes 2020
+    </div>
+  );
 }
 
 export default Footer;
